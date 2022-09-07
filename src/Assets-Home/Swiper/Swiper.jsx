a90805a6b8da044995881e7ca11777ea92fd1473
Nandito06 <maklonspoto@gmail.com>
2022-09-07 23:41:17 +0700

    nambah gambar

--- src/Assets-Home/Swiper/Swiper.jsx
+++ src/Assets-Home/Swiper/Swiper.jsx
@@ -13,46 +13,91 @@ export default () => {
     <Swiper
       spaceBetween={50}
       slidesPerView={3}
-    
+
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>   
+      <SwiperSlide>
         <div class="card" >
-            <img src={slide3} class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div></SwiperSlide>
+          <img src={slide3} class="card-img-top" alt="" />
+          <div class="card-body">
+          <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+          </div>
+            <h5 class="card-title">Manggo</h5>
+            <p class="card-text">Rp 6000</p>
+            <a href="#" class="btn btn-warning btn-costum">Detail</a>
+          </div>
+        </div></SwiperSlide>
       <SwiperSlide> <div class="card" >
-            <img src={slide3} class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div></SwiperSlide>
+        <img src={slide3} class="card-img-top imgTR" alt="" />
+        <div class="card-body">
+          <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+          </div>
+          <h5 class="card-title">Apel</h5>
+          <p class="card-text"> Rp 5000</p>
+          <a href="#" class="btn btn-warning btn-costum">Detail</a>
+        </div>
+      </div></SwiperSlide>
       <SwiperSlide> <div class="card" >
-            <img src={slide3} class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div></SwiperSlide>
+        <img src={slide3} class="card-img-top" alt="" />
+        <div class="card-body">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+          </div>
+          <h5 class="card-title">Pepaya</h5>
+          <p class="card-text">Rp 10.000</p>
+          <a href="#" class="btn btn-warning btn-costum">Detail</a>
+        </div>
+      </div>
+      </SwiperSlide>
       <SwiperSlide> <div class="card" >
-            <img src={slide3} class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div></SwiperSlide>
-      ...
+        <img src={slide3} class="card-img-top" alt="" />
+        <div class="card-body">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+          </div>
+          <h5 class="card-title">Sawi</h5>
+          <p class="card-text">Rp 3000</p>
+          <a href="#" class="btn btn-warning btn-costum">Detail</a>
+        </div>
+      </div></SwiperSlide>
+      <SwiperSlide> <div class="card" >
+        <img src={slide3} class="card-img-top" alt="" />
+        <div class="card-body">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+          </div>
+          <h5 class="card-title">Cabai</h5>
+          <p class="card-text">Rp 2000</p>
+          <a href="#" class="btn btn-warning btn-costum">Detail</a>
+        </div>
+      </div></SwiperSlide>
+      
+      
     </Swiper>
   );
 };
