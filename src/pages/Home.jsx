922455659759aba24376de59b789d9683a99ce03
[Nandito06] <[maklonspoto@gmail.com]>
2022-09-07 13:02:53 +0700

    progres1 swiper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -123,14 +123,29 @@ export default function Home() {
             <br />
             <p className='deskripsi'>100% Organic, tanpa Pestisida</p>
           </div>
-
-
+        </div>
+      </div>
+      {/* fitur end */}
+      <div className='Top-recomed'>
+        <div className='container'>
+        <div className='heading'>Top Recomended</div>
+        <div className='produk1'>
+          <div class="card" >
+            <img src="" class="card-img-top" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
         </div>
 
 
+
+
+
       </div>
-
-
+      </div>
     </div>
   )
 }
