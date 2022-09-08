2bb8676ff5694eca231d8bb400daff1093dfaabb
Nandito06 <maklonspoto@gmail.com>
2022-09-08 12:25:13 +0700

    pengerjaan page produk 1

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,21 @@
+import sayuran from '../Assets-Home/Img Swiper/sayuran.png'
+import buahan from  '../Assets-Home/Img Swiper/buahan.png'
 export default function Product() {
   return (
-    <div>Product</div>
+    <div>
+      <div className='container text-center'>
+        <div className='row '>
+          
+          <div className='col-md-6'style={{backgroundColor:'red'}} >
+
+      <img src={sayuran} alt="" className='imgproduk'/>
+          </div>
+          <div className='col-md-6'style={{backgroundColor:'yellow'}} >
+
+          <img src={buahan} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
