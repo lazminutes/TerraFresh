cab4c61534ff08aa770aeb664bdd57ea3d53e2ff
Nandito06 <maklonspoto@gmail.com>
2022-09-09 11:09:24 +0700

    Fix bug di halaman home bagian fitur

--- src/Pages-produk-tambahan/Buah.jsx
+++ src/Pages-produk-tambahan/Buah.jsx
@@ -4,60 +4,12 @@ export default function Buah() {
     return (
         <>
        <Navbar/>
-       <div>
+       <div className="container">
         <div className='card-title'>
           <h1>Buah Segar Hari ini</h1>
           </div>
-          <div className='Sayur d-flex'>
+          <div className='Sayur d-flex text-center'>
       <div class="card" >
-        <div className='item'>
-          <img src={sawi} class="card-img-top" alt="" />
-          </div>
-          <div class="card-body">
-          <div class="stars">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-          </div>
-
-            <h5 class="card-title">Manggo</h5>
-            <p class="card-text">Rp 6000</p>
-            <div className='shopnow'>
-            <a href="#" ><button class="btn-Costum">Detail</button>  </a> 
-           
-            <i className="fas fa-shopping-cart" />
-            </div>
-            <p>stock:10</p>
-           
-          </div>
-        </div>
-        <div class="card" >
-        <div className='item'>
-          <img src={sawi} class="card-img-top" alt="" />
-          </div>
-          <div class="card-body">
-          <div class="stars">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-          </div>
-
-            <h5 class="card-title">Manggo</h5>
-            <p class="card-text">Rp 6000</p>
-            <div className='shopnow'>
-            <a href="#" ><button class="btn-Costum">Detail</button>  </a> 
-           
-            <i className="fas fa-shopping-cart" />
-            </div>
-            <p>stock:10</p>
-           
-          </div>
-        </div>
-        <div class="card" >
         <div className='item'>
           <img src={sawi} class="card-img-top" alt="" />
           </div>
