7da2206aa598afc3aefef74541c6084c4be320fc
Dika <dikarian475@gmail.com>
2022-09-10 15:05:19 +0800

    push pertama

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,5 +1,21 @@
+import React from 'react'
+import "./style.css";
+import ava from "../assets/ava.png"
+
+
 export default function Payment() {
   return (
-    <div>Payment</div>
+        <>
+       {/*Container*/}
+       <div className="container d-lg-flex" style={{borderRadius: '15px'}}>
+        {/* Box 1 */}
+        <div className="box-1 bg-light user" style={{marginTop: '20px'}}> 
+        <div className="d-flex align-items-center mb-3">
+            <img src={ava} className="pic rounded-circle" alt="avatar" />
+            <p className="ps-2 name">User</p>
+        </div>
+        </div>
+        </div>
+    </>
   )
 }
