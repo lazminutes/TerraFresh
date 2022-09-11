22e24a07213e917fdc94c7dc82da14fd404205b2
Nandito06 <maklonspoto@gmail.com>
2022-09-11 21:18:11 +0700

    Logo Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,52 @@
+import logo from '../assets/Img/logo2.png'
 import { Link, NavLink } from "react-router-dom";
+import '../Assets-Home/Home-Css/home.css'
 
 export default function Navbar() {
   return (
     <>
-     <nav class="navbar navbar-expand-lg bg-warning">
+     <nav class="navbar navbar-expand-lg bg-warning ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <img src={logo} alt="" className='logo' />
+    
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
         <li class="nav-item">
         <li className="nav-item">
-        <NavLink className="nav-link" to="/">
+        <NavLink className="nav-link text-light" to="/">
           Home
         </NavLink>
       </li>
         </li>
 
         <li className="nav-item">
-        <NavLink className="nav-link" to="/product">
+        <NavLink className="nav-link text-light" to="/product">
           Product
         </NavLink>
       </li>
 
       <li className="nav-item">
-        <NavLink className="nav-link" to="/about">
+        <NavLink className="nav-link text-light" to="/about">
           About
         </NavLink>
       </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/cart">
+      <li className="nav-item ">
+        <NavLink className="nav-link text-light" to="/cart">
           Cart
         </NavLink>
       </li>
       </ul>
+      
+      <form class="d-flex bg-warning " role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline-light" type="submit" >Search</button>
+      </form>
       <a className="text-reset me-3" to="#">
-      <i className="fas fa-shopping-cart" />
+      <i className="fas fa-shopping-cart text-light " />
     </a>
       <a
         className="text-reset me-3 dropdown-toggle hidden-arrow"
@@ -47,15 +56,31 @@ export default function Navbar() {
         data-mdb-toggle="dropdown"
         aria-expanded="false"
       >
-        <i className="fas fa-bell" />
+        <i className="fas fa-bell text-light" />
         <span className="badge rounded-pill badge-notification bg-danger">
           1
         </span>
       </a>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-success" type="submit" >Search</button>
-      </form>
+      <div class="dropdown">
+  <img 
+  class="btn btn-secondary dropdown-toggle" 
+  type="button"
+   data-bs-toggle="dropdown"
+    aria-expanded="false"
+    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+          className="rounded-circle"
+          height={25}
+          alt="Black and White Portrait of a Man"
+          loading="lazy"/>
+   
+ 
+  <ul className="dropdown-menu dropdown-menu-end"
+        aria-labelledby="navbarDropdownMenuAvatar">
+    <li><a class="dropdown-item" href="#">My Profile</a></li>
+    <li><a class="dropdown-item" href="#">Setting</a></li>
+    <li><a class="dropdown-item" href="#">Log Out</a></li>
+  </ul>
+</div>
     </div>
   </div>
 </nav>
