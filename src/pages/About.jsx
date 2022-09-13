e0fdf8376d378f9711e0303aa88fecf8941dbc8b
Nandito06 <maklonspoto@gmail.com>
2022-09-13 13:53:04 +0700

    tambah Link webdi about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
 
           <div className='Link'>
            
-          <a href=""> 
+          <a href="https://nando-portofolio.netlify.app/" target="_blank"> 
            
             <img src="https://nando-portofolio.netlify.app/static/media/foto.b2867b0ab5404a6aeff3.jpg" alt="" className="anggota nimate__animated animate__bounceIn animate__delay-1s" />
               </a>
@@ -26,8 +26,8 @@ export default function About() {
          
 
           <div className='Link'>
-          <a href=""> 
-            <img src="https://media-exp1.licdn.com/dms/image/C5603AQHYdn6uFgam9A/profile-displayphoto-shrink_800_800/0/1649523686513?e=2147483647&v=beta&t=7b0lOS_gqC9MVD4mo3OAxxU_BqiQyskaBFF7Zd59xTM" alt="" className="anggota nimate__animated animate__bounceIn animate__delay-1s" />
+          <a href="https://cool-tanuki-5aab5a.netlify.app/" target="_blank"> 
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQHYdn6uFgam9A/profile-displayphoto-shrink_800_800/0/1649523686513?e=2147483647&v=beta&t=7b0lOS_gqC9MVD4mo3OAxxU_BqiQyskaBFF7Zd59xTM" alt="" className="anggota nimate__animated animate__bounceIn animate__delay-0s" />
              </a>
             <p className="text-putih">Dika</p>
            
@@ -36,7 +36,7 @@ export default function About() {
 
 
           <div className='Link'>
-          <a href="">
+          <a href=""  target="_blank">
             <img src="https://pps.whatsapp.net/v/t61.24694-24/300754444_783231842930633_1067218521195121380_n.jpg?ccb=11-4&oh=01_AVzvj66P2W-rs2be90MVd13vHBVei_cFvfBeVWpolDmYrg&oe=63305F2D" alt="" className="anggota nimate__animated animate__bounceIn animate__delay-1s" />
              </a>
             <p className="text-putih">Kharmila</p>
@@ -44,8 +44,8 @@ export default function About() {
           </div>
 
 
-          <div className='Link'>
-          <a href="">
+          <div className='Link' >
+          <a href="https://personalwebme.netlify.app/" target="_blank">
             <img src="https://pps.whatsapp.net/v/t61.24694-24/295203360_3229903683926535_3117951425647528933_n.jpg?ccb=11-4&oh=01_AVx-lrETWN3D_MAyF4Sho2kRXfhFSj_fGNzPVsI3TuD3iw&oe=632FA83D" alt="" className="anggota nimate__animated animate__bounceIn animate__delay-1s" />
             </a>
             <p className="text-putih">Putri</p>
@@ -53,8 +53,8 @@ export default function About() {
           </div>
 
     
-          <div className='Link'>
-          <a href="">
+          <div className='Link' >
+          <a href="https://lazarus-s2-technical1.netlify.app/" target="_blank">
             <img src="https://lazarus-s2-technical1.netlify.app/assets/profile_pic.c348e7ee.png" alt="" className="anggota nimate__animated animate__bounceIn animate__delay-1s" />
             </a>
             <p className="text-putih">Lazarus</p>
